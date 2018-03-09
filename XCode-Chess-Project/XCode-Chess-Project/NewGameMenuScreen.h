4683ba46b8096a022fab8b7f3eb66529bd4d1d32
Daniel Meltzer <meltzerdanielebay@gmail.com>
2018-03-09 12:43:53 +0200

    fix merge

--- XCode-Chess-Project/XCode-Chess-Project/NewGameMenuScreen.h
+++ XCode-Chess-Project/XCode-Chess-Project/NewGameMenuScreen.h
@@ -18,8 +18,18 @@ typedef struct _settings_screen {
     SDL_Window *window;
     SDL_Renderer *windowRenderer;
 } SettingsScreen;
-
-SettingsScreen* init_settings_screen(void);
+/**
+ Init all data and textures. NOTE that the data will be presented only when calling wait_for_start
+ */
+SettingsScreen* init_settings_screen();
+/**
+ Frees all resources.
+ */
+void free_settings_screen(SettingsScreen* screen);
+/**
+ Waits for events until the user click the start button, when he does returns the specified events.
+ If the back button was clicked NULL is returned.
+ */
 GameSettings* wait_for_start(SettingsScreen* screen);
 
 #endif /* NewGameMenuScreen_h */
